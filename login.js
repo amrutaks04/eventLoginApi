e38a4ded6c30e5d16a3e5979521b6b9ca185d41d
amruta.ks <amruta.ks2022cse@sece.ac.in>
2024-05-23 18:09:16 +0530

    login/register

---
+++ login.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const Register = require('./schema1.js');
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+
+
+// Database connection
+async function connectToDb() {
+    try {
+        await mongoose.connect('mongodb+srv://amruta:vieFC9VXxVSgoPzM@cluster0.rgbuaxs.mongodb.net/EventManagement?retryWrites=true&w=majority&appName=Cluster0');
+        console.log('DB Connection established');
+        const port = process.env.PORT || 8003;
+        app.listen(port, function () {
+            console.log(`Listening on port ${port}`);
+        });
+    } catch (error) {
+        console.log("Couldn't establish connection");
+        console.log(error);
+    }
+}
+
+connectToDb();
+
+app.post('/register', async function(request, response) {
+    try {
+        const newUser = await Register.create({
+            email: request.body.email,
+            username: request.body.username,
+            password: request.body.password
+        });
+        response.status(201).json({
+            status: 'success',
+            message: 'User created successfully',
+            user: newUser
+        });
+    } catch (error) {
+        console.error('Error creating user:', error);
+        response.status(500).json({
+            status: 'failure',
+            message: 'Failed to create user',
+            error: error.message
+        });
+    }
+});
+app.post('/login',async function(request,response){
+    try{
+        const {username,password}=request.body
+        const user=await Register.findOne({username,password})
+
+        if(user){
+            response.status(200).json({
+                "status":"success",
+                "message":"Valid user"
+            })
+        }
+        else{
+            response.status(401).json({
+                "status":"failure",
+                "message":"Invalid user"
+            })
+        }
+    }
+    
+    catch (error) {
+        console.error('Error fetching users:', error);
+        response.status(500).json({
+          status: 'failure',
+          message: 'Failed to fetch users',
+          error: error.message
+        })
+      }
+})
---
+++ schema1.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const RegisterSchema = new mongoose.Schema({
+
+    email: {
+      type: String,
+      required: true
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
+  });
+  
+  const Register = mongoose.model('Register', RegisterSchema);
+  
+  module.exports = Register;
