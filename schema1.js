3859249bf893a18ce2e051496e34ab2b5bbe2077
amruta.ks <amruta.ks2022cse@sece.ac.in>
2024-06-03 12:23:23 +0530

    git

--- schema1.js
+++ schema1.js
@@ -1,27 +1,20 @@
 const mongoose = require('mongoose');
+const RegisterSchema = new mongoose.Schema({
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  username: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  verified: {
-    type: Boolean,
-    default: false
-  },
-  verificationToken: String
-});
-
-const Register = mongoose.model('Register', userSchema);
-
-module.exports = Register;
+    email: {
+      type: String,
+      required: true
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
+  });
+  
+  const Register = mongoose.model('Register', RegisterSchema);
+  
+  module.exports = Register;s
